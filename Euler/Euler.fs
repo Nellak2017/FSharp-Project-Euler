2f448f35aeeb8b655807de412d3906fce6d49304
Connor Keenum <nellakprime@gmail.com>
2024-12-26 20:48:38 -0600

    First commit. First 3 Euler problems solved.

---
+++ Euler/Euler.fs
@@ -0,0 +1,39 @@
+namespace Euler.Library
+
+// 1. (new concepts: List operations like List.filer and List.sum)
+module SumOfMultiples = 
+    let sumOfMultiples3Or5Below max = 
+        [1..max - 1]
+        |> List.filter (fun x -> x % 3 = 0 || x % 5 = 0)
+        |> List.sum
+
+// 2. (new concepts: Recursive Match Looping)
+module EvenFibonacciNumbers =
+    let evenFib n = 
+        let rec loop a b acc =
+            match a with
+            | _ when a > n -> acc  
+            | _ when a % 2 = 0 -> loop b (a + b) (acc + a)
+            | _ -> loop b (a + b) acc
+        loop 0 1 0 
+
+// 3. (new concepts: Int64, while to rec conversions, tail calls)
+module LargestPrimeFactor = 
+    [<TailCall>] // This will give a compiler warning if the recursion is non-tail call optimizable
+    let rec trialDivLoop (a:int64) (acc:int64) =
+            match a with
+            | _ when a % acc = 0 -> acc
+            | _ -> trialDivLoop a (acc+int64(1))
+    let trialDiv (n:int64) =
+        trialDivLoop n 2
+
+    [<TailCall>]
+    let rec bigPLoop a acc =
+            let trial = trialDiv a
+            match trial with
+            | _ when trial = a -> trial
+            | _ -> bigPLoop (a / trial) (if acc > trial then acc else trial)
+    let bigP (n:int64) =
+        bigPLoop n 1
+
+// 4. ()
---
+++ Euler/Euler_Tests.fs
@@ -0,0 +1,38 @@
+﻿namespace Euler.Tests
+open FsCheck
+open Xunit
+
+module Euler_1 =
+    open Euler.Library.SumOfMultiples
+    [<Fact(Skip = "Done testing")>]
+    let ``f(10) = 23`` () =
+        Assert.Equal(23, sumOfMultiples3Or5Below 10)
+
+    [<Fact(Skip = "Done testing")>]
+    let ``Property Test: f(x) = 0 for x <= 0`` () =
+        let genNegativeOrZero = Gen.choose (-1000, 0) |> Arb.fromGen
+        let prop = Prop.forAll genNegativeOrZero (fun (x: int) -> sumOfMultiples3Or5Below x = 0)
+        Check.One({ Config.Default with MaxTest=100 }, prop)
+        //Check.Quick prop
+
+    [<Fact(Skip = "Done testing")>]
+    let ``Property Test: f(x) > x for x >= 6`` () =
+        let genGreaterThanOrEqual6 = Gen.choose (6, 1000) |> Arb.fromGen
+        let prop = Prop.forAll genGreaterThanOrEqual6 (fun (x: int) -> sumOfMultiples3Or5Below x > x)
+        Check.One({ Config.Default with MaxTest=100 }, prop)
+
+module Euler_2 = 
+    open Euler.Library.EvenFibonacciNumbers
+    [<Fact(Skip = "Done testing")>]
+    let ``f(10) = sum(2;8;34;)`` () =
+        Assert.Equal([2;8;34;] |> List.sum, evenFib 10)
+    // Not many useful properties..
+
+module Euler_3 =
+    open Euler.Library.LargestPrimeFactor
+    [<Fact>]
+    let ``f(13195) = 29`` () =
+        Assert.Equal(29L, bigP 13195L)
+    [<Fact>]
+    let ``f(600851475143) = 6857`` () =
+        Assert.Equal(6857L, bigP 600851475143L)
