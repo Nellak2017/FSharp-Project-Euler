9fe2a00c9339b37f246bf6c5837dfc34a151e2a0
Connor Keenum <nellakprime@gmail.com>
2024-12-26 22:07:56 -0600

    Add Euler 4.

--- Euler/Euler.fs
+++ Euler/Euler.fs
@@ -1,39 +1,51 @@
 namespace Euler.Library
 
 // 1. (new concepts: List operations like List.filer and List.sum)
-module SumOfMultiples = 
-    let sumOfMultiples3Or5Below max = 
-        [1..max - 1]
-        |> List.filter (fun x -> x % 3 = 0 || x % 5 = 0)
-        |> List.sum
+module SumOfMultiples =
+    let sumOfMultiples3Or5Below max =
+        [ 1 .. max - 1 ] |> List.filter (fun x -> x % 3 = 0 || x % 5 = 0) |> List.sum
 
 // 2. (new concepts: Recursive Match Looping)
 module EvenFibonacciNumbers =
-    let evenFib n = 
+    let evenFib n =
         let rec loop a b acc =
             match a with
-            | _ when a > n -> acc  
+            | _ when a > n -> acc
             | _ when a % 2 = 0 -> loop b (a + b) (acc + a)
             | _ -> loop b (a + b) acc
-        loop 0 1 0 
+
+        loop 0 1 0
 
 // 3. (new concepts: Int64, while to rec conversions, tail calls)
-module LargestPrimeFactor = 
+module LargestPrimeFactor =
     [<TailCall>] // This will give a compiler warning if the recursion is non-tail call optimizable
-    let rec trialDivLoop (a:int64) (acc:int64) =
-            match a with
-            | _ when a % acc = 0 -> acc
-            | _ -> trialDivLoop a (acc+int64(1))
-    let trialDiv (n:int64) =
-        trialDivLoop n 2
+    let rec trialDivLoop (a: int64) (acc: int64) =
+        match a with
+        | _ when a % acc = 0 -> acc
+        | _ -> trialDivLoop a (acc + int64 (1))
+
+    let trialDiv (n: int64) = trialDivLoop n 2
 
     [<TailCall>]
     let rec bigPLoop a acc =
-            let trial = trialDiv a
-            match trial with
-            | _ when trial = a -> trial
-            | _ -> bigPLoop (a / trial) (if acc > trial then acc else trial)
-    let bigP (n:int64) =
-        bigPLoop n 1
+        let trial = trialDiv a
 
-// 4. ()
+        match trial with
+        | _ when trial = a -> trial
+        | _ -> bigPLoop (a / trial) (if acc > trial then acc else trial)
+
+    let bigP (n: int64) = bigPLoop n 1
+
+// 4. (new concepts: Int to string, reversing strings, checking string equality, lazy seq syntax)
+module LargestPalindromeProduct =
+    let isPalindrome (n: int) =
+        n.ToString() = System.String(n.ToString() |> Seq.rev |> Array.ofSeq)
+
+    let largestPalindromeProduct () =
+        seq {
+            for i in 999..-1..100 do
+                for j in 999..-1..100 do
+                    yield i * j
+        }
+        |> Seq.filter isPalindrome // Filters to Palindromes
+        |> Seq.max
--- Euler/Program.fs
+++ Euler/Program.fs
@@ -2,12 +2,14 @@
 open Euler.Library.SumOfMultiples
 open Euler.Library.EvenFibonacciNumbers
 open Euler.Library.LargestPrimeFactor
+open Euler.Library.LargestPalindromeProduct
 
 module Program =
     let main args = 
         printfn "Euler 1 is: %d" (sumOfMultiples3Or5Below 1000)
         printfn "Euler 2 is: %d" (evenFib 4000000)
         printfn "Euler 3 is: %d" (bigP 600851475143L)
+        printfn "Euler 4 is: %d" (largestPalindromeProduct ())
         0
     [<EntryPoint>]
     let mainEntryPoint args = main args
